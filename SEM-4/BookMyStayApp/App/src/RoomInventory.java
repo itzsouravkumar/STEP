6969a50ddb3f9a85ed692f4db60b99a34fd8c4f5
itzsouravkumar <oyesourav05012006@gmail.com>
2026-03-15 03:46:16 +0530

    feat: implement UC-4 Room Search and Availability Check

---
+++ SEM-4/BookMyStayApp/App/src/RoomInventory.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class RoomInventory {
+    // HashMap to map room types to their available counts
+    private Map<String, Integer> availabilityMap;
+
+    public RoomInventory() {
+        availabilityMap = new HashMap<>();
+        // Initializing room availability for each room type
+        availabilityMap.put("Single", 5);
+        availabilityMap.put("Double", 3);
+        availabilityMap.put("Suite", 2);
+    }
+
+    public void updateAvailability(String roomType, int count) {
+        if (availabilityMap.containsKey(roomType)) {
+            availabilityMap.put(roomType, availabilityMap.get(roomType) + count);
+        } else {
+            System.out.println("Room type '" + roomType + "' does not exist.");
+        }
+    }
+
+    public int getAvailableRooms(String roomType) {
+        return availabilityMap.getOrDefault(roomType, 0);
+    }
+
+    public void displayInventory() {
+        System.out.println("Current Room Inventory:");
+        for (Map.Entry<String, Integer> entry : availabilityMap.entrySet()) {
+            System.out.println(entry.getKey() + " Rooms Available: " + entry.getValue());
+        }
+    }
+}
