16802df364cb376da912a522f94babc7d3f17d1e
itzsouravkumar <oyesourav05012006@gmail.com>
2026-04-06 14:10:02 +0530

    Sourav added - keep only BookMyStay branch-specific file

--- SEM-4/BookMyStayApp/App/src/InvalidBookingException.java
+++
@@ -1,8 +0,0 @@
-/**
- * Custom exception to represent invalid booking scenarios.
- */
-public class InvalidBookingException extends Exception {
-    public InvalidBookingException(String message) {
-        super(message);
-    }
-}
