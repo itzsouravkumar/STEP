c724610812a7295e9ea467878469911cc8db9e19
itzsouravkumar <oyesourav05012006@gmail.com>
2026-04-06 14:44:40 +0530

    Sourav added - Train dev branch with UC1 to UC5

---
+++ SEM-4/TrainConsistManagementApp/App/src/UseCase5PreserveInsertionOrderBogies.java
@@ -0,0 +1,21 @@
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class UseCase5PreserveInsertionOrderBogies {
+    public static void main(String[] args) {
+        System.out.println("UC5 Preserve Insertion Order of Bogies");
+
+        Set<String> trainFormation = new LinkedHashSet<>();
+        trainFormation.add("Engine");
+        trainFormation.add("Sleeper");
+        trainFormation.add("Cargo");
+        trainFormation.add("Guard");
+        trainFormation.add("Sleeper");
+
+        System.out.println("Final Train Formation:");
+        System.out.println(trainFormation);
+        System.out.println("Note:");
+        System.out.println("LinkedHashSet preserves insertion order and removes duplicates automatically.");
+        System.out.println("UC5 formation setup completed...");
+    }
+}
